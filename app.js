370a978ad9331ccbe850415cdaeb56867e6d92c3
nebeel0 <lee1623@purdue.edu>
2016-12-04 17:15:22 -0500

    Testing different login templates.

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 /**
  * Module dependencies.
  */
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('express-flash');
+const errorHandler = require('errorhandler');
+const expressValidator = require('express-validator');
 
 var express = require('express')
   , http = require('http')
@@ -11,28 +20,91 @@ var app = express();
 var pathname = __dirname + '/public/';
 var appServer = http.createServer(app);
 var io = require('socket.io')(appServer);
-var clients = [];
-var people = {};
-var rooms = {};
 
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.load({ path: '.env.example' });
+
+/**
+ * Controllers (route handlers).
+ */
+const userController = require('./controllers/user');
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connection.on('error', () => {
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// Connect Flash
+app.use(flash());
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+  store: new MongoStore({
+    url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
+    autoReconnect: true
+  })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+app.use((req, res, next) => {
+  // After successful login, redirect back to the intended page
+  if (!req.user &&
+      req.path !== '/login' &&
+      req.path !== '/signup' &&
+      !req.path.match(/^\/auth/) &&
+      !req.path.match(/\./)) {
+    req.session.returnTo = req.path;
+  } else if (req.user &&
+      req.path == '/account') {
+    req.session.returnTo = req.path;
+  }
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
 app.use(function (req, res, next){
 	console.log("/" + req.method);
 	next();
 })
+*/
 
+/*
 app.get("/", function (req,res){
-	res.sendfile(pathname + "index.html");
+  res.sendfile(pathname + "chatService.html");
 })
+*/
+
+
+
 app.get("/chatService", function (req,res){
 	res.sendfile(pathname + "chatService.html");
 })
@@ -49,13 +121,7 @@ io.on('connection', function(socket){
 });
 
 
-//app.get("/", express.static(__dirname + '/public_username'));
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-
+app.use(errorHandler());
 
 // Simple AI Type
 var simpleAI = require('./simpleAI/simpleAI');
